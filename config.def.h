34105cb34d5a4ed454b9f21f69e5c81b806d167a
n0zmu <mickael.munozserrano@gmail.com>
2024-06-01 12:32:19 +0200

    clean

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "firacode:pixelsize=14:antialias=true:autohint=true";
+static char *font = "hack:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
